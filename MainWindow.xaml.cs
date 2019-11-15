9d909ddfdde10f0f223b5f4ca1fdd305183623f8
egorchern <egorch.formal@gmail.com>
2019-11-15 18:07:25 +0000

    fafa

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Project_ChessWithInterface
 {
@@ -23,6 +24,19 @@ namespace Project_ChessWithInterface
         public MainWindow()
         {
             InitializeComponent();
+            string pathToResources = Environment.CurrentDirectory;
+            pathToResources = Regex.Replace(pathToResources, @"\\", "¬");
+            var temp = pathToResources.Split('¬').ToList();
+            temp.RemoveAt(temp.Count - 1);
+            temp.RemoveAt(temp.Count - 1);
+            for(int i = 0; i < temp.Count; i++)
+            {
+                temp[i] += @"\";
+            }
+            pathToResources = String.Join("", temp) + "Resources";
+            double d = 0.0;
+            image1.Source = new BitmapImage(new Uri(pathToResources + "\\WhiteKing.png"));
+            
         }
     }
 }
