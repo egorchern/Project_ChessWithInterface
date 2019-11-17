ecdeb28946612ea0fde497eb502f42d08af14eb9
Egor <egorch.formal@gmail.com>
2019-11-17 20:33:23 +0000

    Base interface

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
+using System.Web.UI.HtmlControls;
+
+
 
 namespace Project_ChessWithInterface
 {
@@ -24,19 +27,108 @@ namespace Project_ChessWithInterface
         public MainWindow()
         {
             InitializeComponent();
+
+            
+
+            
+            GetAllButtonElements();
+
+            Globals.pathToResources = GetPathToResources();
+
+            foreach(Button btn in Globals.AllButtons)
+            {
+                btn.Click += UniversalSquareClickEventHandle;
+            }
+            foreach(string item in Globals.PathsToPieces)
+            {
+                comboBox.Items.Add(item);
+            }
+                
+                
+        }
+
+        private void UniversalSquareClickEventHandle(object sender, RoutedEventArgs e)
+        {
+            
+            string name = ((Button)sender).Name;
+            string pathToPiece = comboBox.SelectedItem.ToString();
+            int index = 0;
+            for(int i = 0; i < Globals.AllButtons.Count; i++)
+            {
+                string scopedName = Globals.AllButtons[i].Name;
+                if(scopedName == name)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            Globals.AllButtons[index].Content = new Image
+            {
+                Source = new BitmapImage(new Uri(Globals.pathToResources + pathToPiece)),
+                VerticalAlignment = VerticalAlignment.Center
+
+            };
+            
+        }
+        public void GetAllButtonElements()
+        {
+            /// casting the content into panel
+            Panel mainContainer = (Panel)this.Content;
+
+            /// GetAll UIElement
+            UIElementCollection element = mainContainer.Children;
+
+            /// casting the UIElementCollection into List
+            List<FrameworkElement> lstElement = element.Cast<FrameworkElement>().ToList();
+
+            /// Geting all Control from list
+            var lstControl = lstElement.OfType<Control>();
+
+            foreach (Control control in lstControl)
+            {
+                Type s = control.GetType();
+                if(s.Name == "Button")
+                {
+                    Globals.AllButtons.Add(control as Button);
+                    
+                }
+               
+            }
+        }
+
+
+        public static string GetPathToResources()
+        {
             string pathToResources = Environment.CurrentDirectory;
             pathToResources = Regex.Replace(pathToResources, @"\\", "¬");
             var temp = pathToResources.Split('¬').ToList();
             temp.RemoveAt(temp.Count - 1);
             temp.RemoveAt(temp.Count - 1);
-            for(int i = 0; i < temp.Count; i++)
+            for (int i = 0; i < temp.Count; i++)
             {
                 temp[i] += @"\";
             }
             pathToResources = String.Join("", temp) + "Resources";
-            double d = 0.0;
-            image1.Source = new BitmapImage(new Uri(pathToResources + "\\WhiteKing.png"));
-            
+            return pathToResources;
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
+
+    public static class Globals
+    {
+        public static int CurrentySelected = 0;
+        public static List<string> PathsToPieces = new List<string> { "\\WhiteRook.png", "\\WhiteKnight.png", "\\WhiteBishop.png", "\\WhiteQueen.png", "\\WhiteKing.png", "\\WhitePawn.png", "\\BlackRook.png", "\\BlackKnight.png", "\\BlackBishop.png", "\\BlackQueen.png", "\\BlackKing.png", "\\BlackPawn.png" };
+        public static string pathToResources = "";
+        public static List<Button> AllButtons = new List<Button>();
+
+       
+    }
+    
+
 }
+
